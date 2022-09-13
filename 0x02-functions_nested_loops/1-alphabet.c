5406c2c44917fbceb768675dbc211425497df9f0
periodtqween <periodtqween@gmail.com>
2022-09-13 10:15:02 -0700

    las

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- *   print_alphabet - function to print abc
- *   
+ *   print_alphabet - function to print abc  
  *   Return: 0
  */
 void print_alphabet(void)
 {
-		char c;
+		char c = 'a';
 
-			for (c = 'a'; c <= 'z'; c++)
-					{
-								_putchar(c);
-									}
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+			}
 			_putchar('\n');
 }
+
 
