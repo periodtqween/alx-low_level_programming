6418f0e4a6f11e092eb01b055377175f1ec0f92b
periodtqween <periodtqween@gmail.com>
2022-10-05 01:46:32 -0700

    compile

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+/**
+ *  * *_strdup - program startup
+ *   * @str: char being evaluated
+ *   (*
+ *    * Description: returns a pointer to a newly allocated space in memory
+ *     * which contains a copy of the string given as a parameter)?
+ *      * Return: return (0) is the required function signature
+ *       */
+
+#include "main.h"
+char *_strdup(char *str)
+{
+		char *array;
+			unsigned int rand;
+				unsigned int strlen = 0;
+
+					if (str == 0)
+							{
+										return (NULL);
+											}
+						while (str[strlen])
+								{
+											strlen++;
+												}
+							strlen++;
+
+								array = malloc(sizeof(char) * strlen);
+
+									if (array == 0)
+											{
+														return (NULL);
+															}
+										for (rand = 0; rand < strlen; rand++)
+												{
+															array[rand] = str[rand];
+																}
+											return (array);
+}
+
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+tartup
+ * @grid: pointer being evaluated
+ *  * @height: int being evaluated
+ *  (*
+		 *   * Description: frees a two dimensional grid previously created
+		 *    * by the `alloc_grid` function)?
+ *     * Return: return (0) is the required function signature
+ *      */
+
+#include "main.h"
+ void free_grid(int **grid, int height)
+{
+		int x;
+
+			for (x = 0; x < height; x++)
+					{
+								free(grid[x]);
+									}
+				free(grid);
+}
+
