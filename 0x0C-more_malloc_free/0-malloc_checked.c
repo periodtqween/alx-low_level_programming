973b391c72527cee43f3ca47c1cd0f5b383f768a
periodtqween <periodtqween@gmail.com>
2022-10-06 08:40:44 -0700

    ha

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,13 +7,13 @@
  *     */
 void *malloc_checked(unsigned int b)
 {
-void *p;
+		void *p;
 
-p = malloc(b);
-	if (!p)
-	{
-	exit(98);
-	}
-	return (p);
+			p = malloc(b);
+				if (!p)
+						{
+									exit(98);
+										}
+					return (p);
 }
 
